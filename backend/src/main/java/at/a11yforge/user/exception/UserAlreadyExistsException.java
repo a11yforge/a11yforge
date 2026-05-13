4e3bc3eff2acbc790e6009eb33b8b8dbdc7f27e5
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-13 21:46:57 +0200

    feat(user): add UserAlreadyExistsException

--- backend/src/main/java/at/a11yforge/user/exception/UserAlreadyExistsException.java
+++ backend/src/main/java/at/a11yforge/user/exception/UserAlreadyExistsException.java
@@ -1,7 +1,8 @@
 package at.a11yforge.user.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
-  public UserAlreadyExistsException(String message) {
-    super(message);
-  }
+
+    public UserAlreadyExistsException(String field, String value) {
+        super("User with " + field + " '" + value + "' already exists");
+    }
 }
