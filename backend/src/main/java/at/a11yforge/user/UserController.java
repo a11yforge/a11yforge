dc69c5da69cc844873bd37605387fc6cebb11b08
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-13 21:45:19 +0200

    feat(user): add UserController

--- backend/src/main/java/at/a11yforge/user/UserController.java
+++ backend/src/main/java/at/a11yforge/user/UserController.java
@@ -1,4 +1,4 @@
-package at.a11yforge.user.exception;
+package at.a11yforge.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
