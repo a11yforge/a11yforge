83d82a77bba099e7d85c5ea311d0e1411325c8e4
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-13 21:47:51 +0200

    feat(user): add UserNotFoundException

--- backend/src/main/java/at/a11yforge/user/exception/UserNotFoundException.java
+++ backend/src/main/java/at/a11yforge/user/exception/UserNotFoundException.java
@@ -1,7 +1,8 @@
 package at.a11yforge.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(String message) {
-    super(message);
-  }
+
+    public UserNotFoundException(Long id) {
+        super("User not found: " + id);
+    }
 }
