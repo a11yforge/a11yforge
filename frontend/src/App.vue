39b196b35bec6de3eb01f498146cf3500bd21c50
Maximilian Mayer <office@maximilianmayer.at>
2026-05-14 20:21:56 +0200

    feat(frontend): add PrimeVue + Tailwind setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="p-8">
+    <h1 class="text-3xl font-bold mb-4">A11yForge</h1>
+    <Button label="Test Button" />
+  </div>
 </template>
-
-<style scoped></style>
