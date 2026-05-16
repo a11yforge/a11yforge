ed9b219f62b51e0e622c1fa3b7153db74bfa79f7
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-16 12:25:21 +0200

    feat(project): add findAllByUserId and findByIdAndUserId queries

--- backend/src/main/java/at/a11yforge/api/project/ProjectRepository.java
+++ backend/src/main/java/at/a11yforge/api/project/ProjectRepository.java
@@ -2,5 +2,11 @@ package at.a11yforge.api.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findAllByUserId(Long userId);
+    Optional<Project> findByIdAndUserId(Long id, Long userId);
 }
