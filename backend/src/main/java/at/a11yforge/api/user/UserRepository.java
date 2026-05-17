cfb5d6daae9b6a431ac5b4d2bcadc1235a223b51
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-17 11:36:30 +0200

    feat(auth): add findByEmail, findByUserName and exists checks to UserRepository

--- backend/src/main/java/at/a11yforge/api/user/UserRepository.java
+++ backend/src/main/java/at/a11yforge/api/user/UserRepository.java
@@ -2,6 +2,14 @@ package at.a11yforge.api.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+
+    boolean existsByEmail(String email);
+    boolean existsByUserName(String userName);
 
 }
