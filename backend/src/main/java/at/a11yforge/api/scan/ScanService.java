dbb571389435a16639a0890dcce9c05691427623
Maximilian Mayer <office@maximilianmayer.at>
2026-05-18 08:05:32 +0200

    feat(scan): add ScanService dependencies and constructor

--- backend/src/main/java/at/a11yforge/api/scan/ScanService.java
+++ backend/src/main/java/at/a11yforge/api/scan/ScanService.java
@@ -1,13 +1,26 @@
 package at.a11yforge.api.scan;
 
+import at.a11yforge.api.page.PageRepository;
+import at.a11yforge.api.project.ProjectRepository;
+import at.a11yforge.api.scanner.ScannerProcessRunner;
+import at.a11yforge.api.violation.ViolationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScanService {
 
     private final ScanRepository scanRepository;
+    private final PageRepository pageRepository;
+    private final ViolationRepository violationRepository;
+    private final ProjectRepository projectRepository;
+    private final ScannerProcessRunner scanner;
 
-    public ScanService(ScanRepository scanRepository) {
+    public ScanService(ScanRepository scanRepository, PageRepository pageRepository, ViolationRepository violationRepository, ProjectRepository projectRepository, ScannerProcessRunner scanner) {
+
         this.scanRepository = scanRepository;
+        this.pageRepository = pageRepository;
+        this.violationRepository = violationRepository;
+        this.projectRepository = projectRepository;
+        this.scanner = scanner;
     }
 }
