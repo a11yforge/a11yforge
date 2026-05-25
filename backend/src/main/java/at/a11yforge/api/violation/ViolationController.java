32a79a279a555aa64463c48527fc23ddac120fc5
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-25 16:07:16 +0200

    feat(violation): add controller for GET violations and violation by id

--- backend/src/main/java/at/a11yforge/api/violation/ViolationController.java
+++ backend/src/main/java/at/a11yforge/api/violation/ViolationController.java
@@ -1,7 +1,14 @@
 package at.a11yforge.api.violation;
 
+import at.a11yforge.api.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/violations")
@@ -11,5 +18,21 @@ public class ViolationController {
 
     public ViolationController(ViolationService violationService) {
         this.violationService = violationService;
+    }
+
+    // HTTP 200 OK
+    @GetMapping
+    public List<ViolationResponseDTO> getViolations(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @RequestParam Long scanId) {
+        return violationService.getViolationsForUser(principal.getId(), scanId);
+    }
+
+    // HTTP 200 OK
+    @GetMapping("/{violationId}")
+    public ViolationResponseDTO getViolation(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @PathVariable Long violationId) {
+        return violationService.getViolationByIdForUser(principal.getId(), violationId);
     }
 }
