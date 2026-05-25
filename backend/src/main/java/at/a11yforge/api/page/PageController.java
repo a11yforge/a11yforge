6cb3aa8bf7afd686c2dd2297926d64441a9d7e34
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-25 13:56:04 +0200

    feat(page): add controller for GET pages and page by id

--- backend/src/main/java/at/a11yforge/api/page/PageController.java
+++ backend/src/main/java/at/a11yforge/api/page/PageController.java
@@ -1,7 +1,14 @@
 package at.a11yforge.api.page;
 
+import at.a11yforge.api.security.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pages")
@@ -11,5 +18,21 @@ public class PageController {
 
     public PageController(PageService pageService) {
         this.pageService = pageService;
+    }
+
+    // HTTP 200 OK
+    @GetMapping
+    public List<PageResponseDTO> getPages(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @RequestParam Long scanId) {
+        return pageService.getPagesForUser(principal.getId(), scanId);
+    }
+
+    // HTTP 200 OK
+    @GetMapping("/{pageId}")
+    public PageResponseDTO getPage(
+            @AuthenticationPrincipal CustomUserDetails principal,
+            @PathVariable Long pageId) {
+        return pageService.getPageByIdForUser(principal.getId(), pageId);
     }
 }
