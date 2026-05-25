6fe8e1ee47f31794d2f40872b3376d9fe224c963
Philipp Reischer <philipp.reischer@gmx.at>
2026-05-25 15:56:37 +0200

    feat(violation): add repository methods with ownership filter

--- backend/src/main/java/at/a11yforge/api/violation/ViolationRepository.java
+++ backend/src/main/java/at/a11yforge/api/violation/ViolationRepository.java
@@ -3,8 +3,15 @@ package at.a11yforge.api.violation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ViolationRepository extends JpaRepository<Violation, Long> {
+
+    //Sollte noch mit findAllByPageScanIdAndPageScanProjectUserId ersetzt werden
     List<Violation> findByPage_Scan_Id(Long scanId);
+
+    List<Violation> findAllByPageScanIdAndPageScanProjectUserId(Long scanId, Long userId);
+
+    Optional<Violation> findByIdAndPageScanProjectUserId(Long id, Long userId);
 
 }
