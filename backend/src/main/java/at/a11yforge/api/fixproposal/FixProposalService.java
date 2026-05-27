e35a540cfbca19e17e25fe6096b003fbe0551fc1
Maximilian Mayer <office@maximilianmayer.at>
2026-05-27 18:21:04 +0200

    feat(fixproposal): inject VerifierProcessRunner into service

--- backend/src/main/java/at/a11yforge/api/fixproposal/FixProposalService.java
+++ backend/src/main/java/at/a11yforge/api/fixproposal/FixProposalService.java
@@ -1,13 +1,16 @@
 package at.a11yforge.api.fixproposal;
 
+import at.a11yforge.api.verifier.VerifierProcessRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FixProposalService {
 
     private final FixProposalRepository fixProposalRepository;
+    private final VerifierProcessRunner verifierProcessRunner;
 
-    public FixProposalService(FixProposalRepository fixProposalRepository) {
+    public FixProposalService(FixProposalRepository fixProposalRepository, VerifierProcessRunner verifierProcessRunner) {
         this.fixProposalRepository = fixProposalRepository;
+        this.verifierProcessRunner = verifierProcessRunner;
     }
 }
