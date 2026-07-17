87f4f64e7b712f9b4e540cda306fd430401a2abf
Philipp Reischer <philipp.reischer@gmx.at>
2026-07-17 16:35:49 +0200

    frontend/src/layouts/PublicLayout.vue

--- frontend/src/layouts/PublicLayout.vue
+++ frontend/src/layouts/PublicLayout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import AppNavbar from '@/components/layout/AppNavbar.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col">
+    <AppNavbar />
     <main class="flex-1">
       <slot />
     </main>
